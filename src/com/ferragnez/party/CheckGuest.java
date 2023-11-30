570e089ce65e3894cb0a86d9a151adae8e15a864
pyroz7 <vincipyroz7@gmail.com>
2023-11-30 14:24:49 +0100

    half

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,11 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        String[] nameGuest = {"Dua Lipa","Paris Hilton", "Manuel Agnelli", "J-Ax",
+                "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
+                "Martina Maccherone", "Rachel Zeilic" };
+        System.out.println(nameGuest);
+
+    }
+}
